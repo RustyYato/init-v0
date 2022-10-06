506771842666e62097e64a230499c36adba72be8
RustyYato <rustyyato@gmail.com>
2022-10-06 13:07:50 -0700

    create the foundational traits to initialize T

--- init/src/lib.rs
+++ init/src/lib.rs
@@ -12,3 +12,31 @@
 
 mod ptr;
 pub use ptr::{Init, Uninit};
+
+/// A trait to initialize a T
+pub trait TryInitialize<T: ?Sized> {
+    /// the error reported by this
+    type Error;
+
+    /// attempt to initialize the pointer
+    ///
+    /// if this function returns Ok, then the ptr was initialized
+    /// if this function returns Err, then the ptr may not be initialized
+    fn try_init(self, ptr: Uninit<T>) -> Result<Init<T>, Self::Error>;
+}
+
+/// A trait to initialize a T
+pub trait Initialize<T: ?Sized>: TryInitialize<T, Error = core::convert::Infallible> {
+    /// initializes the ptr
+    fn init(self, ptr: Uninit<T>) -> Init<T>;
+}
+
+impl<T, I: TryInitialize<T, Error = core::convert::Infallible>> Initialize<T> for I {
+    #[inline]
+    fn init(self, ptr: Uninit<T>) -> Init<T> {
+        match self.try_init(ptr) {
+            Ok(init) => init,
+            Err(err) => match err {},
+        }
+    }
+}
